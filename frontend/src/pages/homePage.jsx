957441c74d1ee53e6c111c5df7feb4ce3d12eb0b
tomasfriz <tomyagustinf@gmail.com>
2023-11-28 15:57:44 -0300

    vinculacion de rutas de las secciones

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -8,13 +8,13 @@ const HomePage = () => {
     <div>
       <div className="my-10 container mx-auto px-4">
         <h1 className="text-2xl">
-          ¡Hola <span className="font-bold">{profile?.userName}!</span>
+          ¡Hola <span className="font-bold">{profile?.userName}</span>!
         </h1>
         <p className="py-2">¿Qué deporte querés hacer hoy?</p>
       </div>
 
-      <div className="bg-main-dark px-4 text-white py-5 space-y-4">
-        <h2 className="text-2xl font-bold">¡Organizá tu partido¡</h2>
+      <div className="bg-main-dark px-3 text-white py-3 space-y-3">
+        <h2 className="text-2xl font-bold">¡Organizá tu partido!</h2>
         <p className="">
           ¡Creá partidos del deporte que más te guste! ¡Elige el lugar, la fecha y
           la hora! ¡Anímate a ser el director de tus propios eventos deportivos!
@@ -22,13 +22,13 @@ const HomePage = () => {
       </div>
       <ul className="container mx-auto grid grid-cols-2 px-4 gap-3 my-10">
         <li className="bg-main-color2 py-12 text-center rounded-xl">
-          <Link to="">Organizar</Link>
+          <Link to="/organize">Organizar</Link>
         </li>
         <li className="bg-main-color1 py-12 text-center rounded-xl">
           <Link to="/teams">Buscar Equipos</Link>
         </li>
         <li className="bg-main-color1 py-12 text-center rounded-xl">
-          <Link to="">Mis Equipos</Link>
+          <Link to="/myTeams">Mis Equipos</Link>
         </li>
         <li className="bg-main-color2 py-12 text-center rounded-xl">
           <Link to="/history">Historial</Link>
