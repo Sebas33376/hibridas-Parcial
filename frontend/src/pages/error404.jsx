0419bbf3c81915c533b5c991955ffb22f080099c
tomasfriz <tomyagustinf@gmail.com>
2024-02-21 13:26:24 -0300

    cambios en error404 y error cuando inicias sesion

--- frontend/src/pages/error404.jsx
+++ frontend/src/pages/error404.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Error404 = () => {
   return (
     <div>
       <h1>Error 404</h1>
       <h2>Pagina no encontrada</h2>
-      <a href="/frontend/src/pages/homePage.jsx">Volver al inicio</a>
+      <Link to="/">Volver al inicio</Link>
     </div>
   );
 };
