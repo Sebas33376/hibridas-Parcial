550c56fec58f4802d615051aafa8d778fd038d02
tomasfriz <tomyagustinf@gmail.com>
2024-02-25 11:28:34 -0300

    rediseño de perfil

--- frontend/src/pages/profile/profilePage.jsx
+++ frontend/src/pages/profile/profilePage.jsx
@@ -1,6 +1,8 @@
 import { useProfile, useSession } from "../../context/SessionContext";
 import { useEffect, useState } from "react";
 import "./ProfilePage.css";
+import { Link } from "react-router-dom";
+import IconBack from "../../icons/IconBack";
 
 
 export default function ProfilePage() {
@@ -26,12 +28,19 @@ export default function ProfilePage() {
 
   return (
     <>
-      <div className="mx-4 my-5">
+      <button type="button" className="bg-main-dark inline-block p-3 ml-5 mt-3 rounded-md">
+        <Link to="/">
+          <span >
+            <IconBack />
+          </span>
+        </Link>
+      </button>
+      <div className="mx-20 my-5">
         <h1 className="my-8 font-bold text-xl">Tú Perfil</h1>
         <div className="mb-4 flex items-start">
           <img
             src={profile?.avatar}
-            alt=""
+            alt="Avatar del usuario"
             className="rounded-full h-24 w-24 object-cover border-4 border-main-color1"
           />
           <h2 className="ml-4 mt-3 font-bold text-[20px]">
@@ -40,7 +49,7 @@ export default function ProfilePage() {
         </div>
 
         <div className="my-6">
-          <h3 className="text-gray-600 text-sm mb-1">Nombre y Apellido</h3>
+          <h3 className="text-gray-600 text-sm mb-1">Nombre y Apellido: </h3>
           {isEditing ? (
             <input
               type="text"
@@ -53,7 +62,7 @@ export default function ProfilePage() {
           )}
         </div>
         <div>
-          <h3 className="text-gray-600 text-sm mb-1">Email</h3>
+          <h3 className="text-gray-600 text-sm mb-1">Mail: </h3>
           {isEditing ? (
             <input
               type="text"
@@ -69,20 +78,20 @@ export default function ProfilePage() {
           {isEditing ? (
             <button
               onClick={handleSaveClick}
-              className="bg-main-color1 text-white px-4 py-2 rounded-md"
+              className="bg-main-color1 text-white px-4 py-2 rounded-md hover:bg-green-600"
             >
               Guardar
             </button>
           ) : (
-            <button onClick={handleEditClick} className="text-main-color1">
+            <button onClick={handleEditClick} className="bg-main-color1 text-white px-4 py-2 rounded-md hover:bg-green-600">
               Editar
             </button>
           )}
         </div>
-        <div className="fixed bottom-20 right-4">
+        <div className="fixed bottom-20 right-4 mx-10 my-5">
           <button
             onClick={onLogOut}
-            className="bg-red-500 text-white px-4 py-2 rounded-md"
+            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-700"
           >
             Cerrar sesión
           </button>
