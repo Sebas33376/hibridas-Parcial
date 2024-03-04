f0b2c48c59925ef1f24114674f3a39f8b14898a5
tomasfriz <tomyagustinf@gmail.com>
2024-03-04 12:20:17 -0300

    ...

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-"# Final de Aplicaciones Híbridas"
--- backend/api/routes/api.route.history.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as controller from "../controllers/api.controller.js";
-import { validateToken } from "../../middleware/token.validate.js";
-
-const apiHistoryRoute = Router();
-
-apiHistoryRoute.get("/history/:id", [validateToken], controller.getHistory);
-
-export default apiHistoryRoute;
